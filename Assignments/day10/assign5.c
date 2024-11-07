5564f7e02ae034de2420592aba8112be92b50dfd
AnjalliBoyapalli <anjali.boyapalli@capgemini.com>
2024-11-07 23:20:03 +0530

    Update assign5.c

--- Assignments/day10/assign5.c
+++ Assignments/day10/assign5.c
@@ -26,3 +26,25 @@ int main() {
 }
 
 -----> For Numericals
+int main() {
+    int rows;
+    printf("Enter no of rows: "); 
+    scanf("%d", &rows);
+
+    for (int i = 0; i < rows; i++) 
+    {
+        for (int j = 0; j < rows - i - 1; j++) 
+        {
+            printf(" ");
+        }
+        for (int j = 0; j >1 ; j++) {
+            printf("%i", j);
+        }
+        for (int j = i - 1; j >= 0; j--) {
+            printf("%i", j);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
